10de359c94453dc9f773a1202fbfe4c10d6c7b11
denis klimov <tyferly2003@gmail.com>
2024-10-01 21:24:02 +0400

    fix ошибки

--- migrations_general/versions/de5b94dcc881_revert_changers.py
+++ migrations_general/versions/de5b94dcc881_revert_changers.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'de5b94dcc881'
-down_revision: Union[str, None] = 'e5635ab09c12'
+down_revision: Union[str, None] = '69ca132e5e49'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
